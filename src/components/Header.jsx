5152ecad94ddbb0421a34343352493b8965c0eb3
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-07 08:56:06 +0300

    button.styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
+import Button from "./styles/Button.styled";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div>
+        <Button color="#A62440">Apply Courses</Button>
+        <Button bg="#A62440">Talk to Advisor</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/styles/Button.styled.jsx
+++ src/components/styles/Button.styled.jsx
@@ -1,6 +1,9 @@
 // escb 👇:
 import styled from "styled-components";
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: ${({ bg }) => bg || "#fff"};
+  color: ${({ color }) => color || "#fff"};
+`;
 
 export default Button;
