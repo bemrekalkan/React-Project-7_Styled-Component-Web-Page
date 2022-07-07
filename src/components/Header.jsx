87516599a87c242bd840ed81b7ae6334439bf6ed
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-07 12:58:38 +0300

    header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import Button from "./styles/Button.styled";
-import StyledHeader from "./styles/Header.styled";
+import StyledHeader, { Logo, Nav } from "./styles/Header.styled";
 const Header = () => {
   return (
     <StyledHeader>
-      <div>
-        <Button color="#A62440">Apply Courses</Button>
-        <Button bg="#A62440">Talk to Advisor</Button>
-      </div>
+      <Nav>
+        {/* 👇 Since we put our pictures in the public folder, we can access these pictures with the relative path method ✔ */}
+        <Logo src="./images/logo.png"></Logo>
+        <div>
+          <Button color="#A62440">Apply Courses</Button>
+          <Button bg="#A62440">Talk to Advisor</Button>
+        </div>
+      </Nav>
     </StyledHeader>
   );
 };
--- src/components/styles/Header.styled.jsx
+++ src/components/styles/Header.styled.jsx
@@ -3,7 +3,20 @@
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
+//! Get background color from header inside colors inside theme 🚩
+background-color: ${({ theme }) => theme.colors.header}
   padding: 4rem 0;
+`;
+
+export const Logo = styled.img`
+  width: 300px;
+  padding: 4rem 0;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export default StyledHeader;
